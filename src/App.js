0529f815de976c6eb62f6b852c7cfccfc1ad7fd5
eriq <e.abonie@gmail.com>
2022-05-31 19:51:49 +0100

    first draft of advice quotes api app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    const [quotes, setQuotes] = useState([]);
+
+    const fetchAdvice = () => {
+    //using axios to get the data from the API then setting it into the state with setQuotes
+    //put the link in an environment variable to hide it from prying eyes
+    //.....just jokes....this is the API https://api.adviceslip.com/advice ... enjoy
+        axios.get(`${process.env.REACT_APP_QUOTES_URL}`)
+        .then((response) => {
+            setQuotes(response.data.slip);
+             
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+//using this hook to ensure the API gets on load only then stops []
+    useEffect(()=> {
+        fetchAdvice();
+    },[]);
+
+    console.log(quotes);
+
+    return(
+        <div className="app">
+            <div className="card">
+            <h1 className="heading">{quotes.advice}</h1>
+            <button className="button" onClick={fetchAdvice}><span>GIVE ME ADVICE!</span></button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
